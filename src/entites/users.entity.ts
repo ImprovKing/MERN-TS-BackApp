abdf8506b08f9593912168a9e7f913830ae100f3
ImprovKing <philipalexander572@gmail.com>
2023-06-27 21:30:32 +0100

    first nest commit finally decided to upload

---
+++ src/entites/users.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany, JoinColumn, OneToOne } from "typeorm"
+import { CartEntity } from "./cart.entity"
+import { OrderEntity } from "./order.entity"
+
+@Entity("customers")
+export class Users {
+   @PrimaryGeneratedColumn()
+   id: number
+
+   @Column()
+   username: string 
+
+   @Column()
+   password: string
+
+   @Column()
+   email : string
+
+   @CreateDateColumn()
+   createdAt : String
+
+   access_token?: any ;
+
+   @UpdateDateColumn()
+   updtedAt : String
+
+   @OneToMany(type => CartEntity, cart => cart.id)
+   @JoinColumn()
+   cart: CartEntity[]
+
+   @OneToOne(type => OrderEntity, order => order.id)
+   @JoinColumn()
+   order : OrderEntity;
+}
